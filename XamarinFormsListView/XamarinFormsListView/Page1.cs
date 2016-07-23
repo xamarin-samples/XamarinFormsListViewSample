7fa11756e554f814a1f4356fe7f391ee7a36b7ea
kobake <kobake@users.sourceforge.net>
2016-07-23 17:42:26 +0900

    Label and ListView

--- XamarinFormsListView/XamarinFormsListView/Page1.cs
+++ XamarinFormsListView/XamarinFormsListView/Page1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -12,10 +13,36 @@ namespace XamarinFormsListView
     {
         public Page1()
         {
+            // 参考：http://www.buildinsider.net/mobile/xamarintips/0038
+
+            // リスト作成.
+            var ar = new ObservableCollection<string>();
+            foreach (var i in Enumerable.Range(0, 100))
+            {
+                ar.Add(string.Format("hoge-{0}", i));
+            }
+
+            // リストビュー作成.
+            var listView = new ListView
+            {
+                ItemsSource = ar
+            };
+
             Content = new StackLayout
             {
+                Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0), // iOSのみ上部にマージンをとる.
                 Children = {
-                    new Label { Text = "Hello ContentPage" }
+                    new Frame
+                    {
+                        Padding = new Thickness(10, 10, 10, 10),
+                        OutlineColor = new Color(1.0, 0.0, 0.0), // 赤枠
+                        BackgroundColor=new Color(1.0, 0.8, 0.8), // ピンク背景
+                        Content = new Label {
+                            Text = "Hello ContentPage!!!!!!!!!",
+                            TextColor =new Color(0.0,0.0,0.0) // 黒文字
+                        }
+                    },
+                    listView
                 }
             };
         }
